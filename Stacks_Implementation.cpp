d1cf32b6db56b7fd4f1d52b0bde2f25a641097c4
Himanshu Rawat <himanshurawat90k@gmail.com>
2023-06-27 21:56:41 +0530

    reversal of linked list usin stack

--- Stacks_Implementation.cpp
+++ Stacks_Implementation.cpp
@@ -1,25 +1,64 @@
 #include <iostream>
 #include <stack>
-#include <string.h>
 using namespace std;
 
-void Reverse(char *c, int n) {
-    stack<char> s;
-    //loop for push
-    for(int i = 0; i<n; i++) {
-        s.push(c[i]);
+struct Node {
+    int data;
+    struct Node* link;
+};
+
+struct Node* head;
+
+void Insert(int x) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->link = NULL;
+    if(head == NULL) {  
+        head = temp;
+        return;
     }
-    //loop for pop
-    for(int i = 0;i<n;i++) {
-        c[i] = s.top();
+    struct Node* temp1 = head;
+    while(temp1->link != NULL) {
+        temp1 = temp1->link;
+    }
+    temp1->link = temp;
+}
+
+void Reverse() {
+    if(head == NULL) return;
+    stack<struct Node*> s;
+    struct Node* temp = head;
+    while(temp != NULL) {
+        s.push(temp);
+        temp = temp->link;
+    }
+    temp = s.top();
+    head = temp;
+    s.pop();
+    while(!s.empty()) {
+        temp->link = s.top();
         s.pop();
+        temp = temp->link;
     }
+    temp->link = NULL;
+}
+
+void Print() {
+    struct Node* temp = head;
+    printf("Node: ");
+    while(temp != NULL) {
+        printf("%d ",temp->data);
+        temp = temp->link;
+    }
+    printf("\n");
 }
 
 int main() {
-    char c[51];
-    printf("Enter a string: ");
-    gets(c);
-    Reverse(c, strlen(c));
-    printf("Output = %s", c);
+    head = NULL;
+    Insert(5);Print();
+    Insert(4);Print();
+    Insert(3);Print();
+    Insert(2);Print();
+    Insert(1);Print();
+    Reverse();Print();
 }
